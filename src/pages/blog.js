289a3d4bda6576f9f7e3fcf74d0b9186f3c7a0fd
zwelinux <zwelinux@gmail.com>
2023-02-09 12:49:26 +0630

    9_1_2023_commit_part_5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,36 +1,38 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
-import { graphql } from 'gatsby'
-
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-query {
-  allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-    nodes {
-      id
-      name
-      publicURL
-      modifiedTime(formatString: "DD MMM YY")
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
+      nodes {
+        frontmatter {
+          title
+          date
+          slug
+        }
+        id
+        excerpt
+      }
     }
   }
-}
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
