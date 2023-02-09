988847c8bbda6ad7c06739dfa51154137f0b2db4
zwelinux <zwelinux@gmail.com>
2023-02-09 10:31:41 +0630

    9_2_2023_commit_part_3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `mysite`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
