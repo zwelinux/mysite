780fade434defb678c70ac3d6830de2fc2fdb09b
zwelinux <zwelinux@gmail.com>
2023-02-09 11:28:32 +0630

    9_1_2023_commit_part_4

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,12 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
